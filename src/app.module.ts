4a1c25cab1a43af26138059d8b34ef0616a622a6
Santiago Fajardo <santiago.fajardo@instaleap.io>
2025-05-17 13:20:52 -0500

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArilineModule } from './ariline/ariline.module';
+import { AirportModule } from './airport/airport.module';
 
 @Module({
-  imports: [],
+  imports: [ArilineModule, AirportModule],
   controllers: [AppController],
   providers: [AppService],
 })
